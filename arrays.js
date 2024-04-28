24ada9fabda5e831e1bcaa6f93c268eea6385272
Miguel Simoneto <despsimonetti@gmail.com>
2024-04-28 16:03:27 -0300

    array inicio

---
+++ arrays.js
@@ -0,0 +1,23 @@
+let alunosEM1 = ["vitor","claudio","chico","tadeu","miguel","claudio"]
+console.log(typeof alunosEM1)
+console.log(alunosEM1.length)
+console.log (alunosEM1)
+console.table(alunosEM1)
+console.log(alunosEM1[4])
+alunosEM1.push("jorge")
+console.table(alunosEM1)
+alunosEM1.unshift("luiza")
+console.table(alunosEM1)
+alunosEM1.splice(5, 0, "inserido3", "inserido4", "inserido5")
+console.table(alunosEM1)
+alunosEM1[13]="Leandro"
+console.table(alunosEM1)
+alunosEM1[1]="victor hugo"
+console.table(alunosEM1)
+alunosEM1.pop()
+console.table(alunosEM1)
+alunosEM1.splice(2, 10)
+console.table(alunosEM1)
+
+
+
